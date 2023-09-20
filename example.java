6f3a0830b7942f39d9a0ea19b5167a813b396b65
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-09-20 13:48:39 +0530

    Add files via upload

---
+++ example.java
@@ -0,0 +1,21 @@
+class sample
+ {
+	{
+	   System.out.println("i am praveen");	
+	}
+
+    sample()
+	{
+	   System.out.println("its is an java progarm");
+	} 
+ }
+
+
+class democons1
+  {
+     public static void main(String asd[])
+	{
+	    System.out.println("hello everyone");
+	    sample obj=new sample();
+	}
+ }
