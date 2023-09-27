a108758fd18a6405e4994473c4f3b1f18ff183e3
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-09-27 16:12:58 +0530

    Add files via upload

---
+++ pack2.java
@@ -0,0 +1,18 @@
+package pack;
+
+public class pack2 
+   {
+     String name;
+     int age;
+     public pack2(String name,int age)
+     {
+        this.name=name;
+        this.age=age;
+     }
+     public void displaypack2()
+     {
+        System.out.println("name="+name);
+        System.out.println("age="+age);
+     }
+    
+  }
