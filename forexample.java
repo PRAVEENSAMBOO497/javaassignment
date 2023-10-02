06f3e668d2022e9ff19317f90cb9a4d348834921
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-02 15:44:42 +0530

    Add files via upload

---
+++ forexample.java
@@ -0,0 +1,19 @@
+class sample
+{
+    int i;
+    
+  sample()
+  {
+    for(i=0;i<10;i++)
+    {
+        System.out.println(i);
+     }
+  }
+}
+class forexample
+{
+    public static void main(String atr[])
+    {
+        sample obj = new sample();
+    }
+}
