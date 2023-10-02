d736385681149de25e94c57c997b2659799b535c
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-02 12:39:14 +0530

    Add files via upload

---
+++ demological1.java
@@ -0,0 +1,31 @@
+
+class sample
+ {
+    int x=10,y=12;
+    int z=20;
+    sample()
+	{
+	   System.out.println ((x<y)&&(y<z));
+
+	   if(x<y)
+	    {
+	       System.out.println("true");
+	    }
+	   else
+	   {
+	       System.out.println("false");
+	   }
+	
+	
+	}
+
+ }
+
+
+class demological1
+ {
+    public static void main(String asd[])
+	{
+	   sample obj=new sample();
+	}
+ }
