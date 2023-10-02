fd163969b18b49d31035295f1ed543775e3deebb
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-02 14:51:26 +0530

    Add files via upload

---
+++ whileexample.java
@@ -0,0 +1,19 @@
+
+class whileexample
+{
+    public static void main(String ag[])
+    {
+        int i=1;
+        
+        System.out.println("enter the Number");
+        
+        while(i<=10)
+        {
+           System.out.println("Answer is correct");
+           i++;
+           
+        }
+        
+
+    }
+}
