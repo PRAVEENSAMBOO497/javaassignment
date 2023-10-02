005cca287554cd802549188f416c4633948e48ba
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-02 12:14:18 +0530

    Add files via upload

---
+++ ifandifelse.java
@@ -0,0 +1,29 @@
+class sample 
+{
+  
+    sample()
+    {
+       int age =22;  
+        
+      if(age<30)
+      {
+        System.out.println("age is less then of 20");
+      }
+      if(age>10)
+      {
+        System.out.println("LHS is greater then of RHS ");
+      }
+      else 
+      {
+        System.out.println("no");
+      }
+    }
+}
+class ifandifelse
+{
+    public static void main(String ade[])
+    {
+        sample obj = new sample();
+
+    }
+}
