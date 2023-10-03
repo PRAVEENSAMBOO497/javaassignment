f31a7b6bb9f55846f3e52040052302df41fbb240
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-03 14:41:08 +0530

    Add files via upload

---
+++ realtime2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class realtime2
+{
+  public static void main(String arg[])
+  {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("enter the name of the place");
+     String s1= sc.nextLine();
+     System.out.println(":enter the cost");
+     String s2 = sc.nextLine();
+     System.out.println("Enter the time");
+     int x =sc. nextInt();
+     System.out.println("Enter the destination");
+     String s3=sc.nextLine();
+
+  }
+}
