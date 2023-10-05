0a0ff7a19d15ffbf67858c0cba1759aeb601f760
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-05 12:29:19 +0530

    Add files via upload

---
+++ arrayexample1.java
@@ -0,0 +1,13 @@
+class arrayexample1
+{
+    public static void main(String ad[])
+    {   int x[]= new int[10];
+        int y[]=new int[20];
+         System.out.println(x[1]);
+         System.out.println("hello");
+    }
+
+    
+
+    
+}
