99c77f745065d0dc49e95226c14f45624d1c8a81
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 15:18:04 +0530

    Add files via upload

---
+++ lengthexample.java
@@ -0,0 +1,15 @@
+ class lengthexample 
+  {
+    public static void main(String ayd[])
+    {
+        String s3="i18n sollution";
+        String s4="I am praveen";
+        System.out.println("length() method returns length of the specified String.");
+
+        System.out.println(s3.length());
+        System.out.println(s4.length());
+
+        System.out.println("Everthing ok");
+
+    }
+  }
