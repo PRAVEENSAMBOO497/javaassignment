91dec013142ae58f24229edc8cae74c7b6ceb8db
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 13:17:12 +0530

    Add files via upload

---
+++ stringdelete.java
@@ -0,0 +1,9 @@
+class stringdelete
+    {
+       public static void main(String ga[])
+       {
+        StringBuffer sb = new StringBuffer("Sathish_muthu");
+        sb.delete(0, 7);
+        System.out.println(sb);
+       }
+    }
