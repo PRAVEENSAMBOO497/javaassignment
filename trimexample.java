bd209ad47d1a43ade71e90b8bb673a14145be839
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 14:51:23 +0530

    Add files via upload

---
+++ trimexample.java
@@ -0,0 +1,10 @@
+class trimexample 
+    {
+        public  static void main(String atg[])
+        {
+            String s1= "    Praveen";
+            System.out.println(s1.trim());
+            System.out.println(s1);
+            System.out.println("eliminates white spaces before and after the String.");
+        }
+    }
