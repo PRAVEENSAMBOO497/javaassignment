d034800a3195d6a84fa28ac64fb5eeaefd4cd986
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 12:01:31 +0530

    Add files via upload

---
+++ radixexample.java
@@ -0,0 +1,15 @@
+
+public class radixexample 
+{
+    public static void main(String[] args)
+     {
+        int number = 42;
+
+        String formatter = new String();
+        
+        System.out.println("Current Radix: " + number);
+
+        
+
+    }
+}
