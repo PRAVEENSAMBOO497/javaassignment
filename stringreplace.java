59fb604364737f37c6a0355f2d2ed5495c607b12
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 13:10:19 +0530

    Add files via upload

---
+++ stringreplace.java
@@ -0,0 +1,9 @@
+class stringreplace
+{
+    public static void main(String au[])
+    {
+        StringBuffer sb=new StringBuffer("Praveen");
+        sb.replace(0, 0,  " and suraj");
+        System.out.println(sb);
+    }
+}
