b0a45105079a8682dbf3284ac734d0e012294e99
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 14:44:28 +0530

    Add files via upload

---
+++ touppercaseexample.java
@@ -0,0 +1,11 @@
+class touppercaseexample
+ {
+    public static void main(String as[])
+    {
+        String sc="Praveen";
+        String s1="Suraj";
+        System.out.println(sc.toUpperCase());
+        System.out.println(s1.toLowerCase());
+        System.out.println(sc);
+    }
+ }
