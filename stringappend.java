96ee86ba03989946d6934456f3a26e894df26170
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 12:38:49 +0530

    Add files via upload

---
+++ stringappend.java
@@ -0,0 +1,11 @@
+class stringappend
+{
+    public static void main(String ar[])
+    {
+        StringBuffer sb= new StringBuffer("hello");
+        sb.append(" Everyone");
+        System.out.println(sb);
+        System.out.println("  ");
+
+    }
+}
