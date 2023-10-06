d26cb051691d01aacc2ca4f56c4517206639eb5d
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 14:59:50 +0530

    Add files via upload

---
+++ startsWithendwhithexample.java
@@ -0,0 +1,10 @@
+ class startsWithendwhithexample 
+    {
+       public static void main(String af[])
+       {
+          String s1="It is an java program";
+          System.out.println(s1.startsWith("ad"));
+          System.out.println(s1.endsWith("am"));
+          System.out.println("hello");
+       }
+    }
