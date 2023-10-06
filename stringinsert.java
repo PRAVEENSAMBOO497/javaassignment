85fcec6ebfe7bf7be5bd4e8b7acdaf4e9f3a582b
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 12:53:06 +0530

    Add files via upload

---
+++ stringinsert.java
@@ -0,0 +1,16 @@
+class sample
+{
+    sample()
+      {
+        StringBuffer sb=new StringBuffer("Hariprasath");
+        sb.insert(9,"n");
+        System.out.println(sb);
+      }
+}
+class stringinsert
+{
+    public static void  main(String ah[])
+    {
+      new sample();
+    }
+}
