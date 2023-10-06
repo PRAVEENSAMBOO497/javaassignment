8095464524a4e3f5fd5b3466d99492a39e0899ec
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 15:05:42 +0530

    Add files via upload

---
+++ charatexample.java
@@ -0,0 +1,10 @@
+class charatexample 
+     {
+         public static void main(String ahe[])
+         {
+            String s2="Praveen";
+            System.out.println(" charAt() method returns a character at specified index.");
+            System.out.println(s2.charAt(4));
+            System.out.println(s2.charAt(6));
+         }
+     }
