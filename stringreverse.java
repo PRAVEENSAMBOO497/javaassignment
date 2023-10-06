53440ce59ee3b3e16d8862f62505955d60d8612f
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-06 13:24:10 +0530

    Add files via upload

---
+++ stringreverse.java
@@ -0,0 +1,9 @@
+class stringreverse 
+   {
+    public static void main(String ay[])
+    {
+        StringBuffer sb = new StringBuffer("Praveen");
+        sb.reverse();
+        System.out.print(sb);
+    }
+   }
