b102a22bf84107c64d8e1beb5aa88a80689d3293
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-07 10:02:55 +0530

    Add files via upload

---
+++ comparetoexample.java
@@ -0,0 +1,16 @@
+ class comparetoexample 
+   {
+       public static void main(String ar[])
+       {
+        String s1="Praveen";
+        String s2="Hari";
+        String s3="Muthu";
+        String s4="Suarj";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s2.compareTo(s3));
+        System.out.println(s3.compareTo(s1));
+        System.out.println(s4.compareTo(s1));
+
+        System.out.println("everything ok");
+       }
+   }
