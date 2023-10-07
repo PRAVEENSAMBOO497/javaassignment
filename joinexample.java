df4473d64cbbcce86a7e9e156e1bbaf86756dc03
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-07 11:04:24 +0530

    Add files via upload

---
+++ joinexample.java
@@ -0,0 +1,11 @@
+class joinexample
+    {
+        public static void main(String afd[])
+        {
+            String str="Praveen";
+            str=String.join("hari","suraj","hello");
+              System.out.println(str);
+                
+              
+        }
+    }
