a8298028ba57a43502c3d9c4a8fca4fb7adfcb5e
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-07 10:30:29 +0530

    Add files via upload

---
+++ concatexample.java
@@ -0,0 +1,12 @@
+ class concatexample
+   {
+      public static void main(String ar[])
+      {
+        String s1="Hari is an software enginer";
+
+        s1.concat("really hari is an enginer");
+          System.out.println(s1);
+          s1=s1.concat("hello ");
+          System.out.println(s1);
+      }
+   }
