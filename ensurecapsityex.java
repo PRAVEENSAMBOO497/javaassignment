5caf685a8f32ed05244716d6d887250d8a31bea9
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-10 13:09:59 +0530

    Add files via upload

--- ensurecapsityex.java
+++ ensurecapsityex.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class ensurecapsityex
  {    
     public static void main(String arg[])
-     {   
+    {   
                    
            Vector<Integer> vec = new Vector<>(100);         
              
-           vec.ensureCapacity(25);   
+           //vec.ensureCapacity(25);   
            vec.ensureCapacity(200);        
            
            System.out.println("Minimum capacity of this vector: " +vec.capacity());  
-         }              
+    }              
 }  
