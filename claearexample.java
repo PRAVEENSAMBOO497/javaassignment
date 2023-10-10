c71527a771ad7798c21675fb7151ca4dd3f49c7c
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-10 16:09:37 +0530

    Add files via upload

---
+++ claearexample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class claearexample  
+    {
+        public static void main(String atr[])
+        {
+            Vector <String> sc = new Vector<String>(0, 0);
+            sc.add("I18n sollution");
+            sc.add("Tekfilo innovetive sollution");
+               System.out.println("The insititue name and company name" + sc);
+               sc.clear();
+               System.out.println("finish" + sc);
+        }
+    }
