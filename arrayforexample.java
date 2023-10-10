311198779bf550e01319f0a97ec5afe2463b6289
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-10 05:48:44 +0530

    Add files via upload

---
+++ arrayforexample.java
@@ -0,0 +1,13 @@
+class arrayforexample
+{
+    public static void main(String ag[])
+    {
+    
+            int arr[]={50,60,70,80};  
+            for(int i=0;i<=arr.length;i++)
+            {  
+            System.out.println(arr[i]);  
+            }  
+    } 
+}  
+    
