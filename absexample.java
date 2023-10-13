26417b4871924ee4ff18976fcd54a3cd9f2580bd
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 15:01:25 +0530

    Add files via upload

---
+++ absexample.java
@@ -0,0 +1,25 @@
+class sample
+   {
+       sample()
+       {
+         int x= 23;
+         int y=25;
+              System.out.println(Math.abs(x));
+              System.out.println(Math.abs(y));
+
+            System.out.println(Math.max(x,y));
+            
+            System.out.println(Math.min(x,y));
+              
+            System.out.println("EVERYTHING IS OK ");
+       }
+   }
+public class absexample
+{
+    public static void main(String ar[])
+    {
+        sample obj= new sample();
+        
+
+    }
+}
