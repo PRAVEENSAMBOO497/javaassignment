2731405422bd70c28d2a1b171b78f21f04fccb79
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 13:15:28 +0530

    Add files via upload

---
+++ roundexample.java
@@ -0,0 +1,20 @@
+class sample
+{
+    sample()
+    {
+        float x=12.6f;
+        double y=20.23d;
+         
+         System.out.println(Math.round(x));
+         System.out.println(Math.round(y));
+            
+         System.out.println(("Everything ok"));
+    }
+}
+public class roundexample
+{
+    public static void main(String atr[])
+    {
+        sample obj= new sample();
+    }
+}
