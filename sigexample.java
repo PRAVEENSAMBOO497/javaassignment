d1b4c83c11cdd2bf07c38e74080b522dd3fc6175
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 15:50:33 +0530

    Add files via upload

---
+++ sigexample.java
@@ -0,0 +1,12 @@
+public class sigexample  
+{  
+    public static void main(String[] args)   
+    {  
+        double a= 82.7; 
+        int x = -22;
+        float y= 54.99f; 
+        System.out.println(Math.signum(a));
+        System.out.println(Math.signum(y)); 
+        System.out.println(Math.signum(x));
+    }  
+}  
