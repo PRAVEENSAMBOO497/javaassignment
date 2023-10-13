43deb44520d9b06b6ca128a75d0f4217cab750bc
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 15:18:12 +0530

    Add files via upload

---
+++ cuberootexample.java
@@ -0,0 +1,13 @@
+ class cuberootexample   
+   {
+      public static void main(String ard[])
+      {
+        int x = 12;
+        float y= 22.88f;
+        double z = 22.11d;
+          
+          System.out.println("x cube root is " + Math.cbrt(x));
+          System.out.println("Y cube root is " + Math.cbrt(y));
+          System.out.println( "Z cube root is "+Math.cbrt(z));
+      }
+   }
