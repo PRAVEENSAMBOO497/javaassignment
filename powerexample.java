9139315c57f3a53855ef15e60820daca0c9d2e4b
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 15:33:48 +0530

    Add files via upload

---
+++ powerexample.java
@@ -0,0 +1,14 @@
+class powerexample  
+   {
+      public static void main(String ard[])
+      {
+        int x = 12;
+        float y= 22.88f;
+        double z = 22.11d;
+          
+          System.out.println("x power is " + Math.pow(x,y));
+          System.out.println("Y power is " + Math.pow(y,z));
+            System.out.println("ok");
+
+      }
+   }
