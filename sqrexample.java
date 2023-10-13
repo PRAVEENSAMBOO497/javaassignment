020fa4b0a76e8ec39f7559c3d047b90c49dc5a86
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 14:37:55 +0530

    Add files via upload

---
+++ sqrexample.java
@@ -0,0 +1,15 @@
+public class sqrexample 
+    {
+        public static void main(String ard[])
+        {
+            double x= 12.55d;
+            int y=9;
+            float z= 12.55f;
+
+              System.out.println(Math.sqrt(x));
+              System.out.println(Math.sqrt(y));
+              System.out.println(Math.sqrt(z));
+              
+                System.out.println("Ok");
+        }
+    }
