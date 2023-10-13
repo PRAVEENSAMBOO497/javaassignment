c7d3bf34695c29a8937cf2e096d303208bc22a5e
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-13 14:51:59 +0530

    Add files via upload

---
+++ cubeexample.java
@@ -0,0 +1,14 @@
+ class cubeexample
+   {
+     public static  void main(String ar[])
+     {
+        double x= 122d;
+        int y=233;
+        float z= 222.22f;
+
+           System.out.println(Math.cbrt(x));
+           System.out.println(Math.cbrt(y));
+           System.out.println(Math.cbrt(z));
+
+     }
+   }
