86260a4a99e88b8dfc8b6ab10368fd6c1dad79b0
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-14 11:40:15 +0530

    Add files via upload

---
+++ randomexample.java
@@ -0,0 +1,12 @@
+ class randomexample  
+    {
+        public static void main(String atr[])
+        {
+           System.out.println(Math.random());
+           System.out.println(Math.random());
+           System.out.println(Math.random());
+            
+           //finding the random values 
+            
+        }
+    }
