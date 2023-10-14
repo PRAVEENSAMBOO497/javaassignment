20bb6a5e2784b96d423f7201452a927a5118123d
PRAVEEN . A <142213331+PRAVEENSAMBOO497@users.noreply.github.com>
2023-10-14 10:10:21 +0530

    Add files via upload

---
+++ ceilexample.java
@@ -0,0 +1,20 @@
+class sample
+{
+    sample()
+    {
+       int x=12;
+       double y = 32.3d;
+       float z = 54.7f; 
+
+         System.out.println(Math.ceil(x));
+         System.out.println(Math.ceil(y));
+         System.out.println(Math.ceil(z));
+    }
+}
+class ceilexample
+{
+public static void main(String ard[])
+{
+   sample obj=new sample();
+}
+}
